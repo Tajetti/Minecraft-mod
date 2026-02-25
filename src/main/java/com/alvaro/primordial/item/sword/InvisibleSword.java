353cc60949d52213e9787982cc5defc60dc51f31
Alvaro Tajetti <talhettialvaro@gmail.com>
2026-02-25 17:06:58 -0300

    feat: add enchantment to invisible sword

--- src/main/java/com/alvaro/primordial/item/sword/InvisibleSword.java
+++ src/main/java/com/alvaro/primordial/item/sword/InvisibleSword.java
@@ -1,12 +1,31 @@
 package com.alvaro.primordial.item.sword;
 
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.level.Level;
 
 public class InvisibleSword extends SwordItem {
 
     public InvisibleSword(Tier tier, int attackDamageModifier, float attackSpeedModifier, Item.Properties properties ) {
         super(tier, attackDamageModifier, attackSpeedModifier, properties);
+    }
+
+
+    @Override
+    public void inventoryTick(ItemStack stack, Level level, Entity entity, int slot, boolean selected) {
+        if (level.isClientSide) return;
+
+        CompoundTag tag = stack.getOrCreateTag();
+        if (tag.getBoolean("primordialAutoEnchant")) return;
+
+        stack.enchant(Enchantments.SHARPNESS, 5);
+        stack.enchant(Enchantments.UNBREAKING, 3);
+
+        tag.putBoolean("primordialAutoEnchant", true);
     }
 }
